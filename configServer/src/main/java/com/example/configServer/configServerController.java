bb1effde4b3cd0c7778adf560f02b268b8e20a9b
HabibaGamil <habiba.gamil2000@gmail.com>
2023-04-27 22:09:39 +0200

    Congig sever initialized

---
+++ configServer/src/main/java/com/example/configServer/configServerController.java
@@ -0,0 +1,4 @@
+package com.example.configServer;
+
+public class configServerController {
+}
--- test_app/src/main/java/com/example/test_app/controller/TestAppController.java
+++ test_app/src/main/java/com/example/test_app/controller/TestAppController.java
@@ -6,7 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestAppController {
 
-    @RequestMapping("/") public String hello() {
+    @RequestMapping("/addCommand") public String  addCommand() {
+        return "add command\n";
+    }
+    @RequestMapping("/deleteCommand") public String deleteCommand() {
+        return "hello\n";
+    }
+    @RequestMapping("/updateCommand") public String updateCommand() {
+        return "hello\n";
+    }
+    @RequestMapping("/updateClass") public String updateClass() {
+
         return "hello\n";
     }
 
