785e6d7e909e3311ea45b110602793044e8a5c6d
HabibaGamil <habiba.gamil2000@gmail.com>
2023-05-05 21:21:04 +0300

    Controller initialized

---
+++ controller/src/main/java/com/example/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/test")
+    public String hello(){
+        return "Hello from Controller";
+    }
+}
