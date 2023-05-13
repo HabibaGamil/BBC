dd067cf398b6aa4b5a826ce94c3ec233d2855834
SalmaKhalidShreef <salmakh200070@gmail.com>
2023-05-13 12:09:51 +0300

    Request Response class ex

---
+++ test_app/src/main/java/com/example/test_app/RabbitMQ/RequestResponse.java
@@ -0,0 +1,13 @@
+package com.example.test_app.RabbitMQ;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestResponse {
+    String response;
+}
