097732b6a7c516988c36b88eab2662f487bb8c56
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2023-05-28 22:09:26 +0300

    add dependencies to docker-compose

--- test_app/src/main/java/com/example/test_app/TestAppApplication.java
+++ test_app/src/main/java/com/example/test_app/TestAppApplication.java
@@ -3,6 +3,9 @@ package com.example.test_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 
 @SpringBootApplication
@@ -11,6 +14,12 @@ public class TestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
+
+
+	}
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
 	}
 
 
